c413aef58b7c81e7873031b434761b1a2b4572c9
Carlos Fernandes <carlosfernandes@iamachine.europe-west2-c.c.iovirtualmachine.internal>
2020-11-10 13:06:42 +0000

    split into training and test set

--- classification/KNN1_5parks_1hour.py
+++ classification/KNN1_5parks_1hour.py
@@ -18,22 +18,61 @@ from datetime import datetime
 import pytz
 import pandas as pd
 from sqlalchemy import create_engine
+import matplotlib.pyplot as plt
 import pymysql
 
 db_connection_str = 'mysql+pymysql://root:test@localhost/dissertation'
 db_connection = create_engine(db_connection_str)
 
-df = pd.read_sql('SELECT * FROM final_data_5parks_1hour', con=db_connection)
+accepted_columns = ['parkName', 'date', 'occupancy', 'isWeekend', 'isHoliday', 'precipIntensity',
+                    'precipProbability', 'temperature', 'apparentTemperature',
+                    'windSpeed', 'place1Type', 'place2Type', 'place3Type', 'place4Type', 'place5Type',
+                    'parkingGroupName', 'numberOfSpaces', 'hourlyPrice']
 
-##df['occupancy'].describe()
+drop_cols = []
 
+dataset = pd.read_sql('SELECT * FROM final_data_5parks_1hour', con=db_connection)
+
+
+for col in dataset.columns.tolist():
+    if col not in accepted_columns:
+        drop_cols.append(col)
+
+
+## REMOVE UNUSED COLS
+dataset.drop(columns=drop_cols, inplace=True, axis=1)
+
+
+##dataset['occupancy'].describe()
+
+## SET CLASSES ACCORDING OCCUPANCY
 bins = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-
 group_names = ['class1', 'class2', 'class3', 'class4', 'class5', 'class6', 'class7', 'class8', 'class9', 'class10']
-print(pd.cut(df['occupancy'], bins, labels=group_names).value_counts())
-df['class'] = pd.cut(df['occupancy'], bins, labels=group_names)
+##print(pd.cut(dataset['occupancy'], bins, labels=group_names).value_counts())
 
-print(df['class'])
+dataset['class'] = pd.cut(dataset['occupancy'], bins, labels=group_names)
+##dataset.insert(0, 'class', pd.cut(dataset['occupancy'], bins, labels=group_names))
+
+dataset.drop(columns=['occupancy'], inplace=True, axis=1)
+
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+
+
+## SPLITIING INTO TRAINING SET AND TEST SET
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+
+print(X_train)
+
+
+
+
+
+
+
 
 
 
