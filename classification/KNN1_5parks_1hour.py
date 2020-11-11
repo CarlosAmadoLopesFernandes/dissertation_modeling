7e19ec07c92244f7272521ab794cb33c9f231f75
Carlos Fernandes <carlosfernandes@iamachine.europe-west2-c.c.iovirtualmachine.internal>
2020-11-11 21:53:48 +0000

    knn first model

--- classification/KNN1_5parks_1hour.py
+++ classification/KNN1_5parks_1hour.py
@@ -17,6 +17,7 @@ import mysql.connector
 from datetime import datetime
 import pytz
 import pandas as pd
+import numpy as np
 from sqlalchemy import create_engine
 import matplotlib.pyplot as plt
 import pymysql
@@ -24,14 +25,18 @@ import pymysql
 db_connection_str = 'mysql+pymysql://root:test@localhost/dissertation'
 db_connection = create_engine(db_connection_str)
 
-accepted_columns = ['parkName', 'date', 'occupancy', 'isWeekend', 'isHoliday', 'precipIntensity',
-                    'precipProbability', 'temperature', 'apparentTemperature',
+accepted_columns = ['parkName', 'day', 'month', 'year', 'hour', 'weekDay', 'occupancy', 'isWeekend', 'isHoliday', 'precipIntensity',
+                  'precipProbability', 'temperature', 'apparentTemperature',
                     'windSpeed', 'place1Type', 'place2Type', 'place3Type', 'place4Type', 'place5Type',
                     'parkingGroupName', 'numberOfSpaces', 'hourlyPrice']
+
+###accepted_columns = ['parkName', 'occupancy', 'parkingGroupName']
 
 drop_cols = []
 
 dataset = pd.read_sql('SELECT * FROM final_data_5parks_1hour', con=db_connection)
+
+
 
 
 for col in dataset.columns.tolist():
@@ -43,29 +48,146 @@ for col in dataset.columns.tolist():
 dataset.drop(columns=drop_cols, inplace=True, axis=1)
 
 
-##dataset['occupancy'].describe()
-
-## SET CLASSES ACCORDING OCCUPANCY
-bins = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+'''bins = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 group_names = ['class1', 'class2', 'class3', 'class4', 'class5', 'class6', 'class7', 'class8', 'class9', 'class10']
-##print(pd.cut(dataset['occupancy'], bins, labels=group_names).value_counts())
+print(pd.cut(dataset['occupancy'], bins, labels=group_names).value_counts())
+dataset['class'] = pd.cut(dataset['occupancy'], bins, labels=group_names)'''
 
-dataset['class'] = pd.cut(dataset['occupancy'], bins, labels=group_names)
+
 ##dataset.insert(0, 'class', pd.cut(dataset['occupancy'], bins, labels=group_names))
 
+
+## SET CLASSES ACCORDING OCCUPANCY
+dataset['class'] = pd.cut(dataset['occupancy'], bins=10)
+##print(dataset['class'].unique())
+
+'''
+GENERATED CLASSES
+    0: (-0.1, 10]
+    1: (10, 20]
+    2: (20, 30]
+    3: (30, 40]
+    4: (40, 50]
+    5: (50, 60]
+    6: (60, 70]
+    7: (70, 80]
+    8: (80, 90]
+    9: (90, 100]
+'''
+
+
 dataset.drop(columns=['occupancy'], inplace=True, axis=1)
+
+
+## ENCODING CATEGORICAL DATA WITH DUMMY APPROACH
+dataset = pd.get_dummies(dataset, columns=['parkName', 'parkingGroupName', 'place1Type', 'place2Type', 'place3Type', 'place4Type','place5Type'], drop_first=True)
+
+
+## ENCODE TARGET VARIABLE WITH LABEL ENCODER
+from sklearn.preprocessing import LabelEncoder
+lb_make = LabelEncoder()
+dataset['class_code'] = lb_make.fit_transform(dataset['class'])
+
+
+##print(dataset[["class", "class_code"]].head(50))
+#DROP COLUMN CLASS
+dataset.drop(columns=['class'], inplace=True, axis=1)
+
+
+print(list(dataset.columns))
+
+
+
+##TAKING CARE OF MISSING DATA
+'''
+    THE MISSING DATA IN precipIntensity and precipProbability is on summer, between 19 May 2018 - 29 August 2018
+    So nan values assume previous val
+    THE IMPACT ON MEAN WAS TESTED AND ITS MINIMUM
+    #print(dataset['precipIntensity'].mean())
+    #print(dataset['precipProbability'].mean())
+    FOR temperature, apparentTemperature and windSpeed the used approach was the same because weather conditions are stable
+    
+'''
+
+dataset['precipIntensity'].fillna(method='pad', inplace=True)
+dataset['precipProbability'].fillna(method='pad', inplace=True)
+dataset['temperature'].fillna(method='pad', inplace=True)
+dataset['apparentTemperature'].fillna(method='pad', inplace=True)
+dataset['windSpeed'].fillna(method='pad', inplace=True)
+
+##print(dataset.isnull().sum())
 
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
 
-
+print("split dataset")
 ## SPLITIING INTO TRAINING SET AND TEST SET
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
 
 
-print(X_train)
+
+print("training model")
+#TRAINING THE KNN MODEL ON THE TRAINING SET
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors=5, metric='minkowski', p=2)
+classifier.fit(X_train, y_train)
+
+#PREDICTING A NEW RESULT
+##print(classifier.predict(sc.transform([[30, 87000]])))
+
+print("predicting result")
+## PREDICTING THE TEST SET RESULT
+y_pred = classifier.predict(X_test)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+
+print("MAKING CONFUSION MATRIX")
+##MAKING THE CONFUSION MATRIX
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+print(accuracy_score(y_test, y_pred))
+print("DONE ***********")
+
+'''
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = sc.inverse_transform(X_train), y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 1),
+                     np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 1))
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('K-NN (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = sc.inverse_transform(X_test), y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 1),
+                     np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 1))
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('K-NN (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()'''
+
+
+
+
 
 
 
