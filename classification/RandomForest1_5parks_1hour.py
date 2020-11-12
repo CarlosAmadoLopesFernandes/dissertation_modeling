396720c384347b6bfaff61539acf9d0c1f2d2c17
Carlos Fernandes <carlosfernandes@iamachine.europe-west2-c.c.iovirtualmachine.internal>
2020-11-12 17:01:37 +0000

    random forest classification

---
+++ classification/RandomForest1_5parks_1hour.py
@@ -0,0 +1,116 @@
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+
+db_connection_str = 'mysql+pymysql://root:test@localhost/dissertation'
+db_connection = create_engine(db_connection_str)
+
+accepted_columns = ['parkName', 'day', 'month', 'year', 'hour', 'weekDay', 'occupancy', 'isWeekend', 'isHoliday',
+                    'precipIntensity',
+                    'precipProbability', 'temperature', 'apparentTemperature',
+                    'windSpeed', 'place1Type', 'place2Type', 'place3Type', 'place4Type', 'place5Type',
+                    'parkingGroupName', 'numberOfSpaces', 'hourlyPrice']
+
+
+drop_cols = []
+
+dataset = pd.read_sql('SELECT * FROM final_data_5parks_1hour', con=db_connection)
+
+for col in dataset.columns.tolist():
+    if col not in accepted_columns:
+        drop_cols.append(col)
+
+## REMOVE UNUSED COLS
+dataset.drop(columns=drop_cols, inplace=True, axis=1)
+
+'''bins = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+group_names = ['class1', 'class2', 'class3', 'class4', 'class5', 'class6', 'class7', 'class8', 'class9', 'class10']
+print(pd.cut(dataset['occupancy'], bins, labels=group_names).value_counts())
+dataset['class'] = pd.cut(dataset['occupancy'], bins, labels=group_names)'''
+
+
+
+## SET CLASSES ACCORDING OCCUPANCY
+dataset['class'] = pd.cut(dataset['occupancy'], bins=10)
+dataset.drop(columns=['occupancy'], inplace=True, axis=1)
+
+## ENCODING CATEGORICAL DATA WITH DUMMY APPROACH
+dataset = pd.get_dummies(dataset, columns=['parkName', 'parkingGroupName', 'place1Type', 'place2Type', 'place3Type',
+                                           'place4Type', 'place5Type'], drop_first=True)
+
+## ENCODE TARGET VARIABLE WITH LABEL ENCODER
+from sklearn.preprocessing import LabelEncoder
+
+lb_make = LabelEncoder()
+dataset['class_code'] = lb_make.fit_transform(dataset['class'])
+
+##print(dataset[["class", "class_code"]].head(50))
+# DROP COLUMN CLASS
+dataset.drop(columns=['class'], inplace=True, axis=1)
+
+print(list(dataset.columns))
+
+##TAKING CARE OF MISSING DATA
+
+dataset['precipIntensity'].fillna(method='pad', inplace=True)
+dataset['precipProbability'].fillna(method='pad', inplace=True)
+dataset['temperature'].fillna(method='pad', inplace=True)
+dataset['apparentTemperature'].fillna(method='pad', inplace=True)
+dataset['windSpeed'].fillna(method='pad', inplace=True)
+
+##print(dataset.isnull().sum())
+
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+print("split dataset")
+## SPLITIING INTO TRAINING SET AND TEST SET
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+print("training model")
+# TRAINING THE RANDOM FOREST CLASSIFICATION MODEL ON THE TRAINING SET
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators=100, criterion='entropy', random_state=0)
+classifier.fit(X_train, y_train)
+
+# PREDICTING A NEW RESULT
+##print(classifier.predict(sc.transform([[30, 87000]])))
+
+print("predicting result")
+## PREDICTING THE TEST SET RESULT
+y_pred = classifier.predict(X_test)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+
+print("MAKING CONFUSION MATRIX")
+##MAKING THE CONFUSION MATRIX
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+print(accuracy_score(y_test, y_pred))
+print("DONE ***********")
+
+'''
+[[   0    2    1    5   13   38   51   27   16    6]
+ [   0    0    9   21   45   91  116  103   41   36]
+ [   1    8   18   52  144  287  367  280  155   95]
+ [   5    6   32  106  286  724  843  667  326  231]
+ [   6   11   65  211  641 1318 1650 1189  574  442]
+ [   7   27  106  322  889 1869 2297 1799  860  666]
+ [   9   29  104  343  985 2160 2601 1967  885  739]
+ [   4   30   92  329  849 1885 2380 1716  845  661]
+ [   7   25   56  191  583 1296 1637 1208  593  418]
+ [   9   12   65  190  558 1119 1436 1067  506  395]]
+0.15818837547572082
+DONE ***********
+Process finished with exit code 0
+
+'''
+
+
+
+
+
+
