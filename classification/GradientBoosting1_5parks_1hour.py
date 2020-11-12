310c41ec301429e94763ee07a0f9d0ff2f55fa25
Carlos Fernandes <carlosfernandes@iamachine.europe-west2-c.c.iovirtualmachine.internal>
2020-11-12 17:26:47 +0000

    gradient boosting added

---
+++ classification/GradientBoosting1_5parks_1hour.py
@@ -0,0 +1,123 @@
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+
+db_connection_str = 'mysql+pymysql://root:test@localhost/dissertation'
+db_connection = create_engine(db_connection_str)
+
+accepted_columns = ['parkName', 'day', 'month', 'year', 'hour', 'weekDay', 'occupancy', 'isWeekend', 'isHoliday',
+                    'precipIntensity',
+                    'precipProbability', 'temperature', 'apparentTemperature',
+                    'windSpeed', 'place1Type', 'place2Type', 'place3Type', 'place4Type', 'place5Type',
+                    'parkingGroupName', 'numberOfSpaces', 'hourlyPrice']
+
+
+drop_cols = []
+
+dataset = pd.read_sql('SELECT * FROM final_data_5parks_1hour', con=db_connection)
+
+for col in dataset.columns.tolist():
+    if col not in accepted_columns:
+        drop_cols.append(col)
+
+## REMOVE UNUSED COLS
+dataset.drop(columns=drop_cols, inplace=True, axis=1)
+
+'''bins = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+group_names = ['class1', 'class2', 'class3', 'class4', 'class5', 'class6', 'class7', 'class8', 'class9', 'class10']
+print(pd.cut(dataset['occupancy'], bins, labels=group_names).value_counts())
+dataset['class'] = pd.cut(dataset['occupancy'], bins, labels=group_names)'''
+
+
+
+## SET CLASSES ACCORDING OCCUPANCY
+dataset['class'] = pd.cut(dataset['occupancy'], bins=10)
+dataset.drop(columns=['occupancy'], inplace=True, axis=1)
+
+## ENCODING CATEGORICAL DATA WITH DUMMY APPROACH
+dataset = pd.get_dummies(dataset, columns=['parkName', 'parkingGroupName', 'place1Type', 'place2Type', 'place3Type',
+                                           'place4Type', 'place5Type'], drop_first=True)
+
+## ENCODE TARGET VARIABLE WITH LABEL ENCODER
+from sklearn.preprocessing import LabelEncoder
+
+lb_make = LabelEncoder()
+dataset['class_code'] = lb_make.fit_transform(dataset['class'])
+
+##print(dataset[["class", "class_code"]].head(50))
+# DROP COLUMN CLASS
+dataset.drop(columns=['class'], inplace=True, axis=1)
+
+print(list(dataset.columns))
+
+##TAKING CARE OF MISSING DATA
+
+dataset['precipIntensity'].fillna(method='pad', inplace=True)
+dataset['precipProbability'].fillna(method='pad', inplace=True)
+dataset['temperature'].fillna(method='pad', inplace=True)
+dataset['apparentTemperature'].fillna(method='pad', inplace=True)
+dataset['windSpeed'].fillna(method='pad', inplace=True)
+
+##print(dataset.isnull().sum())
+
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+print("split dataset")
+## SPLITIING INTO TRAINING SET AND TEST SET
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+print("training model")
+# TRAINING THE GRADIENT BOOST CLASSIFICATION MODEL ON THE TRAINING SET
+from sklearn.ensemble import GradientBoostingClassifier
+classifier = GradientBoostingClassifier(n_estimators=10, criterion='friedman_mse', loss='deviance', learning_rate=0.1, random_state=0)
+classifier.fit(X_train, y_train)
+
+# PREDICTING A NEW RESULT
+##print(classifier.predict(sc.transform([[30, 87000]])))
+
+print("predicting result")
+## PREDICTING THE TEST SET RESULT
+y_pred = classifier.predict(X_test)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+
+print("MAKING CONFUSION MATRIX")
+##MAKING THE CONFUSION MATRIX
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+print(accuracy_score(y_test, y_pred))
+print("DONE ***********")
+
+'''
+n_estimators=10, criterion='friedman_mse', loss='deviance', learning_rate=0.1, random_state=0
+
+
+MAKING CONFUSION MATRIX
+[[   0    0    0    0    0    0  159    0    0    0]
+ [   0    0    0    0    0    1  460    1    0    0]
+ [   0    0    0    0    0    6 1399    1    0    1]
+ [   0    1    0    0    0   15 3203    5    0    2]
+ [   1    2    0    0    0   24 6070    7    2    1]
+ [   1    0    0    2    2   30 8792   14    0    1]
+ [   0    0    0    0    7   36 9761   15    1    2]
+ [   0    1    0    1    2   34 8737   15    0    1]
+ [   1    1    0    0    2   24 5979    6    1    0]
+ [   1    1    0    0    0   27 5318    7    2    1]]
+0.1954290951840118
+DONE ***********
+
+Process finished with exit code 0
+
+
+
+'''
+
+
+
+
+
+
