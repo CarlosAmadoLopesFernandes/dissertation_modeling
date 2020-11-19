c3c42a0ec1eeed25f06e70a6c33874f53d43beb3
Carlos Fernandes <carlosfernandes@iamachine.europe-west2-c.c.iovirtualmachine.internal>
2020-11-19 03:17:41 +0000

    config module added and predic future route added

---
+++ Config/configs.py
@@ -0,0 +1,28 @@
+config = {
+    "mysql": {
+        "dev": {
+            "host": "localhost",
+            "user": "root",
+            "password": "test"
+        },
+        "prod": {
+            "host": "localhost",
+            "user": "root",
+            "password": "test"
+        }
+    },
+    "dark_sky": {
+        "api_key": "302b25673cfabed60ce47bebdda64afd",
+    },
+    "google": {
+        "api_key": "AIzaSyAdTtHN9lGip2TNWhUUPoPAL1kVWZCqehY"
+    },
+    "general": {
+        "brussels_latitude": 50.85045,
+        "brussels_longitude": 4.34878
+    },
+    "getfestivo": {
+        "api_key": "1ec38d49-81a6-4d6b-9f69-7ecf2c589dab"
+    }
+
+}
--- api/server.py
+++ api/server.py
@@ -6,6 +6,9 @@ import numpy as np
 from datetime import datetime
 import mysql.connector
 from pytz import timezone
+import requests
+from Config import configs
+
 
 
 app = Flask(__name__)
@@ -13,59 +16,23 @@ CORS(app)
 model = pickle.load(open('naive_bayes.pkl', 'rb'))
 
 mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="test"
+    host=configs.config['mysql']['dev']['host'],
+    user=configs.config['mysql']['dev']['user'],
+    password=configs.config['mysql']['dev']['password']
 )
 
 mycursor = mydb.cursor()
-
-
-
-'''
-park,
-date,
-'''
-
 
 schema = Kanpai.Object({
     "park": Kanpai.String().required("Park is required"),
     "date": Kanpai.String().required("Date is required")
 })
 
-'''schema = Kanpai.Object({
-    "day": Kanpai.Number().required("day is required"),
-    "month": Kanpai.Number().required("month is required"),
-    "year": Kanpai.Number().required("year is required"),
-    "hour": Kanpai.Number().required("hour is required"),
-    "weekDay": Kanpai.Number().required("weekDay is required"),
-    "isWeekend": Kanpai.Number().required("isWeekend is required"),
-    "isHoliday": Kanpai.Number().required("isHoliday is required"),
-    "precipIntensity": Kanpai.Number().required("precipIntensity is required"),
-    "precipProbability": Kanpai.Number().required("precipProbability is required"),
-    "temperature": Kanpai.Number().required("temperature is required"),
-    "apparentTemperature": Kanpai.Number().required("apparentTemperature is required"),
-    "windSpeed": Kanpai.Number().required("windSpeed is required"),
-    "numberOfSpaces": Kanpai.Number().required("numberOfSpaces is required"),
-    "hourlyPrice": Kanpai.Number().required("hourlyPrice is required"),
-    "parkName_CCN": Kanpai.Number().required("parkName_CCN is required"),
-    "parkName_Docks Bruxsel": Kanpai.Number().required("parkName_Docks Bruxsel is required"),
-    "parkName_Hippocrate": Kanpai.Number().required("parkName_Hippocrate is required"),
-    "parkName_Lennik": Kanpai.Number().required("parkName_Lennik is required"),
-    "parkingGroupName_Interparking": Kanpai.Number().required("parkingGroupName_Interparking is required"),
-    "parkingGroupName_Q-Park Belgium": Kanpai.Number().required("parkingGroupName_Q-Park Belgium is required"),
-    "parkingGroupName_Rauwers Parkingshop": Kanpai.Number().required("parkingGroupName_Rauwers Parkingshop is required"),
-    "parkingGroupName_SIPE (Indigo Belgium)": Kanpai.Number().required("parkingGroupName_SIPE (Indigo Belgium) is required"),
-    "place1Type_locality": Kanpai.Number().required("place1Type_locality is required"),
-    "place2Type_lodging": Kanpai.Number().required("place2Type_lodging is required"),
-    "place3Type_hospital": Kanpai.Number().required("place3Type_hospital is required"),
-    "place3Type_lodging": Kanpai.Number().required("place3Type_lodging is required"),
-    "place4Type_point_of_interest": Kanpai.Number().required("place4Type_point_of_interest is required"),
-    "place4Type_restaurant": Kanpai.Number().required("place4Type_restaurant is required"),
-    "place4Type_tourist_attraction": Kanpai.Number().required("place4Type_tourist_attraction is required"),
-    "place5Type_lodging": Kanpai.Number().required("place5Type_lodging is required"),
-    "place5Type_point_of_interest": Kanpai.Number().required("place5Type_point_of_interest is required")
-})'''
+schema_future = Kanpai.Object({
+    "latitude": Kanpai.Number().required("Latitude is required"),
+    "longitude":Kanpai.Number().required("Longitude is required"),
+    "date": Kanpai.String().required("Latitude is required"),
+})
 
 
 @app.route('/predict', methods=['POST'])
@@ -73,38 +40,12 @@ def predict():
     try:
         columns = pickle.load(open('model_columns.pkl', 'rb'))
 
-        '''
-            ['parkName', 'day', 'month', 'year', 'hour', 'weekDay',
-            'isWeekend', 'isHoliday', 'precipIntensity', 'precipProbability',
-            'temperature', 'apparentTemperature', 'windSpeed', 'place1Type',
-            'place2Type', 'place3Type', 'place4Type', 'place5Type',
-            'parkingGroupName', 'numberOfSpaces', 'hourlyPrice',
-            'class']
-    
-    
-            ['day', 'month', 'year', 'hour', 'weekDay', 'isWeekend', 'isHoliday',
-            'precipIntensity', 'precipProbability', 'temperature',
-            'apparentTemperature', 'windSpeed', 'numberOfSpaces',
-            'hourlyPrice', 'parkName_CCN',
-            'parkName_Docks Bruxsel',
-            'parkName_Hippocrate',
-            'parkName_Lennik',
-            'parkingGroupName_Interparking', 'parkingGroupName_Q-Park Belgium',
-            'parkingGroupName_Rauwers Parkingshop', 'parkingGroupName_SIPE (Indigo Belgium)',
-            'place1Type_locality', 'place2Type_lodging', 'place3Type_hospital',
-            'place3Type_lodging', 'place4Type_point_of_interest', 'place4Type_restaurant',
-            'place4Type_tourist_attraction', 'place5Type_lodging', 'place5Type_point_of_interest',
-            'class_code']
-        '''
-
-
-
         validation_result = schema.validate(request.json)
         if validation_result.get('success', False) is False:
             return jsonify({
                 "status": "Error",
                 "errors": validation_result.get("error")
-            })
+            }), 400
         data = validation_result.get("data")
         #print(data)
         #data = data.values()
@@ -210,32 +151,12 @@ def predict():
                 else:
                     columns[index] = 0
 
-        model_entry_data = [
-            data["date"].day,
-            data["date"].month,
-            data["date"].year,
-            data["date"].hour,
-            data["date"].weekday(),
-            1 if data["date"].weekday() == 5 or data["date"].weekday() == 6 else 0,
-            1 if holiday_data is not None else 0,
-            meteo_data[7],
-            meteo_data[8],
-            meteo_data[9],
-            meteo_data[10],
-            meteo_data[14],
-            park_data[8],
-            park_data[17]
-        ]
-
-        print(model_entry_data)
+        prediction = model.predict([np.array(columns)])
+        print(prediction)
         print(columns)
-
-        #prediction = model.predict([np.array(data)])
-        #print(prediction)
-        #print(columns)
-        #prediction = {'prediction': str(prediction)}
-        #return jsonify(prediction)
-        return jsonify(columns)
+        prediction = {'prediction': str(prediction)}
+        return jsonify(prediction)
+        #return jsonify(columns)
     except Exception as e:
         if park_data is None or  holiday_data is None or meteo_data is None or places_data is None:
             return jsonify({
@@ -246,4 +167,39 @@ def predict():
                 "error": "Error, try again"
             }), 400
 
+
+@app.route('/predict_future', methods=['POST'])
+def predict_future():
+    print(configs.config)
+    columns = pickle.load(open('model_columns.pkl', 'rb'))
+
+    validation_result = schema_future.validate(request.json)
+    if validation_result.get('success', False) is False:
+        return jsonify({
+            "status": "Error",
+            "errors": validation_result.get("error")
+        }), 400
+    data = validation_result.get("data")
+    date_with_timezone = timezone("Europe/Brussels").localize(data["date"])
+
+    #GET ALL PARKS FROM DATABASE
+    query_parks = "SELECT * FROM `dissertation`.`parks`"
+    mycursor.execute(query_parks)
+    park_data = mycursor.fetchall()
+
+    # GET HOLIDAYS FROM DATABASE WITH GIVEN DATE
+    query_holidays = "SELECT * FROM `dissertation`.`holidays`where date = %s"
+    mycursor.execute(query_holidays, (data["date"].date(),))
+    holiday_data = mycursor.fetchone()
+
+
+    # GET METEO INFO FROM API
+    url = "https://api.darksky.net/forecast/" + configs.config['dark_sky']['api_key'] + "/" + configs.config['general']['brussels_latitude'] + "," + configs.config['general']['brussels_longitude'] + "," + date_with_timezone.timestamp() + "?units=si&exclude=currently,flags"
+    weather_info = requests.get(url).json()
+    if weather_info:
+        print(weather_info)
+    else:
+        print("ERROR retrieving data from darksky")
+
+    return data
 app.run()
