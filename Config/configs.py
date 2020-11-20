80b5a38e08bd7ec32b16d874fa7fb2d061f9dbfa
Carlos Fernandes <carlosfernandes@iamachine.europe-west2-c.c.iovirtualmachine.internal>
2020-11-20 03:15:28 +0000

    scenario 1, returns occupations of parks to given date

--- Config/configs.py
+++ Config/configs.py
@@ -19,10 +19,17 @@ config = {
     },
     "general": {
         "brussels_latitude": 50.85045,
-        "brussels_longitude": 4.34878
+        "brussels_longitude": 4.34878,
+        "used_parks": ['AE979507-05C2-47C2-91BB-61D2F38E13A5', 'EDBE8B15-3E29-45EA-95B0-E7DB1CF48BDE', '0C5DD4C3-01A5-482D-9D71-4E15FCFAA428', '1B5219F5-C3AF-4795-B829-44EBF4974989', 'AA508207-A1E2-4682-A177-9ADD9A07D36A']
     },
     "getfestivo": {
         "api_key": "1ec38d49-81a6-4d6b-9f69-7ecf2c589dab"
+    },
+    "scenarios": {
+      "scenario1": {
+          "weather_variables": ["precipIntensity", "precipProbability", "temperature", "apparentTemperature", "windSpeed"]
+      }
     }
+
 
 }
