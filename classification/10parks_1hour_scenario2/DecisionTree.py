e6d0fe4007cca00590c452ddd464cf706bc01023
Carlos Fernandes <carlosfernandes@iamachine.europe-west2-c.c.iovirtualmachine.internal>
2020-11-24 23:52:40 +0000

    Scenario 2 - Decision tree

---
+++ classification/10parks_1hour_scenario2/DecisionTree.py
@@ -0,0 +1,157 @@
+'''
+
+    MULTI-CLASS CLASSIFICATION
+    Refers to those classification tasks that have more than two class labels.
+
+    Unlike binary classification, multi-class classification does not have the notion of normal and abnormal outcomes.
+    Instead, examples are classified as belonging to one among a range of known classes
+
+    Popular algorithms that can be used for mult-class classification include:
+        . KNN
+        . Decision trees
+        . Naive Bayes
+        . Random Forest
+        . Gradient Boosting
+'''
+import mysql.connector
+from datetime import datetime
+import pytz
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+import matplotlib.pyplot as plt
+import pymysql
+
+db_connection_str = 'mysql+pymysql://root:test@localhost/dissertation'
+db_connection = create_engine(db_connection_str)
+
+accepted_columns = ['parkName', 'day', 'month', 'year', 'hour', 'weekDay', 'occupancy', 'isWeekend', 'isHoliday',
+                    'precipIntensity', 'precipProbability', 'temperature', 'apparentTemperature',
+                    'dewPoint', 'humidity', 'pressure', 'windSpeed', 'windGust', 'windBearing', 'cloudCover',
+                    'uvIndex', 'visibility', 'place1Type', 'place2Type', 'place3Type', 'place4Type', 'place5Type',
+                    'parkingGroupName', 'numberOfSpaces', 'handicappedPlaces', 'hourlyPrice',
+                    'has_car_washing', 'has_bike_sharing', 'has_bike_parking', 'has_elevator', 'has_information_point', 'has_toilet',
+                    'has_petrol_station', 'has_shop', 'has_restaurant', 'has_electric_charging_station', 'has_overnight_accommodation',
+                    'has_kiosk', 'has_pharmacy', 'has_cafe', 'has_medical_facility', 'has_vending_machine', 'has_spare_parts_shopping']
+
+
+drop_cols = []
+
+dataset = pd.read_sql('SELECT * FROM final_data_10parks_1hour', con=db_connection)
+
+for col in dataset.columns.tolist():
+    if col not in accepted_columns:
+        drop_cols.append(col)
+
+## REMOVE UNUSED COLS
+dataset.drop(columns=drop_cols, inplace=True, axis=1)
+
+## SET CLASSES ACCORDING OCCUPANCY
+dataset['class'] = pd.cut(dataset['occupancy'], bins=10)
+
+
+dataset.drop(columns=['occupancy'], inplace=True, axis=1)
+
+## ENCODING CATEGORICAL DATA WITH DUMMY APPROACH
+dataset = pd.get_dummies(dataset, columns=['parkName', 'parkingGroupName', 'place1Type', 'place2Type', 'place3Type',
+                                           'place4Type', 'place5Type'], drop_first=True)
+
+## ENCODE TARGET VARIABLE WITH LABEL ENCODER
+from sklearn.preprocessing import LabelEncoder
+lb_make = LabelEncoder()
+dataset['class_code'] = lb_make.fit_transform(dataset['class'])
+
+
+# DROP COLUMN CLASS
+dataset.drop(columns=['class'], inplace=True, axis=1)
+
+print(list(dataset.columns))
+
+##TAKING CARE OF MISSING DATA
+dataset['precipIntensity'].fillna(method='pad', inplace=True)
+dataset['precipProbability'].fillna(method='pad', inplace=True)
+dataset['temperature'].fillna(method='pad', inplace=True)
+dataset['apparentTemperature'].fillna(method='pad', inplace=True)
+dataset['windSpeed'].fillna(method='pad', inplace=True)
+dataset['humidity'].fillna(method='pad', inplace=True)
+dataset['pressure'].fillna(method='pad', inplace=True)
+dataset['windGust'].fillna(method='pad', inplace=True)
+dataset['windBearing'].fillna(method='pad', inplace=True)
+dataset['cloudCover'].fillna(method='pad', inplace=True)
+dataset['uvIndex'].fillna(method='pad', inplace=True)
+dataset['visibility'].fillna(method='pad', inplace=True)
+
+## WHEN IS NULL, REPLACE BY ZERO
+dataset['handicappedPlaces'].fillna(0, inplace=True)
+
+
+##print(dataset.isnull().sum())
+
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+print("split dataset")
+## SPLITIING INTO TRAINING SET AND TEST SET
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+
+
+print("training model")
+# TRAINING THE DECSISION TREE CLASSIFICATION MODEL ON THE TRAINING SET
+from sklearn.tree import DecisionTreeClassifier
+classifier = DecisionTreeClassifier(criterion='entropy', random_state=0)
+classifier.fit(X_train, y_train)
+
+
+# PREDICTING A NEW RESULT
+##print(classifier.predict(sc.transform([[30, 87000]])))
+
+print("predicting result")
+## PREDICTING THE TEST SET RESULT
+y_pred = classifier.predict(X_test)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+
+print("MAKING CONFUSION MATRIX")
+##MAKING THE CONFUSION MATRIX
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+print(accuracy_score(y_test, y_pred))
+print("DONE ***********")
+
+'''
+MAKING CONFUSION MATRIX
+[[   1    2    8   18   25   34   39   34   35   27]
+ [   4    6   33   59  108  142  174  145   96   72]
+ [   5   41   78  192  318  447  500  417  319  268]
+ [  26   67  204  392  782 1019 1165 1112  710  656]
+ [  36  113  326  774 1310 1979 2115 2011 1366 1206]
+ [  54  183  497 1117 2010 2780 3187 2771 1935 1726]
+ [  54  193  566 1237 2213 3071 3438 3086 2239 1912]
+ [  58  161  446 1170 1913 2817 3109 2748 1884 1666]
+ [  42  123  334  768 1376 1880 2113 1918 1355 1207]
+ [  38  112  317  700 1208 1638 1910 1639 1191 1035]]
+0.14260912967524225
+DONE ***********
+
+Process finished with exit code 0
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
