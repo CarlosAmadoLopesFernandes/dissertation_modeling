34c26abb60ee1d364e0f23a628020b44e4540921
Carlos Fernandes <carlosfernandes@iamachine.europe-west2-c.c.iovirtualmachine.internal>
2020-11-25 22:47:09 +0000

    Scenario 3 - Random Forest

---
+++ classification/10parks_1hour_scenario3/RandomForest.py
@@ -0,0 +1,117 @@
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+
+
+db_connection_str = 'mysql+pymysql://root:test@localhost/dissertation'
+db_connection = create_engine(db_connection_str)
+
+accepted_columns = ['parkName', 'day', 'month', 'year', 'hour', 'weekDay', 'occupancy', 'isWeekend', 'isHoliday',
+                    'place1Type', 'place2Type', 'place3Type', 'place4Type', 'place5Type',
+                    'parkingGroupName', 'numberOfSpaces', 'handicappedPlaces', 'hourlyPrice',
+                    'has_car_washing', 'has_bike_sharing', 'has_bike_parking', 'has_elevator', 'has_information_point', 'has_toilet',
+                    'has_petrol_station', 'has_shop', 'has_restaurant', 'has_electric_charging_station', 'has_overnight_accommodation',
+                    'has_kiosk', 'has_pharmacy', 'has_cafe', 'has_medical_facility', 'has_vending_machine', 'has_spare_parts_shopping']
+
+
+drop_cols = []
+
+dataset = pd.read_sql('SELECT * FROM final_data_10parks_1hour', con=db_connection)
+
+for col in dataset.columns.tolist():
+    if col not in accepted_columns:
+        drop_cols.append(col)
+
+## REMOVE UNUSED COLS
+dataset.drop(columns=drop_cols, inplace=True, axis=1)
+
+## SET CLASSES ACCORDING OCCUPANCY
+dataset['class'] = pd.cut(dataset['occupancy'], bins=10)
+
+
+dataset.drop(columns=['occupancy'], inplace=True, axis=1)
+
+## ENCODING CATEGORICAL DATA WITH DUMMY APPROACH
+dataset = pd.get_dummies(dataset, columns=['parkName', 'parkingGroupName', 'place1Type', 'place2Type', 'place3Type',
+                                           'place4Type', 'place5Type'], drop_first=True)
+
+## ENCODE TARGET VARIABLE WITH LABEL ENCODER
+from sklearn.preprocessing import LabelEncoder
+lb_make = LabelEncoder()
+dataset['class_code'] = lb_make.fit_transform(dataset['class'])
+
+
+# DROP COLUMN CLASS
+dataset.drop(columns=['class'], inplace=True, axis=1)
+
+print(list(dataset.columns))
+
+## WHEN IS NULL, REPLACE BY ZERO
+dataset['handicappedPlaces'].fillna(0, inplace=True)
+
+
+##print(dataset.isnull().sum())
+
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+print("split dataset")
+## SPLITIING INTO TRAINING SET AND TEST SET
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+
+print("training model")
+# TRAINING THE RANDOM FOREST CLASSIFICATION MODEL ON THE TRAINING SET
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators=100, criterion='entropy', random_state=0)
+classifier.fit(X_train, y_train)
+
+
+
+print("predicting result")
+## PREDICTING THE TEST SET RESULT
+y_pred = classifier.predict(X_test)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+
+print("MAKING CONFUSION MATRIX")
+##MAKING THE CONFUSION MATRIX
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+print(accuracy_score(y_test, y_pred))
+print("DONE ***********")
+
+'''
+MAKING CONFUSION MATRIX
+[[   1    1    6   19   18   42   39   42   24   31]
+ [   2    6   22   56  120  159  164  140   93   77]
+ [   5   18   60  154  305  495  550  506  267  225]
+ [  14   42  152  370  718 1103 1312 1115  733  574]
+ [  25   95  262  676 1367 2034 2287 2022 1361 1107]
+ [  35  127  397  941 1968 2938 3474 3005 1819 1556]
+ [  42  136  432 1125 2120 3271 3837 3293 2025 1728]
+ [  37  111  399  989 1917 2972 3270 2871 1890 1516]
+ [  42   96  271  671 1321 2016 2341 1997 1295 1066]
+ [  35   75  248  594 1155 1767 2024 1729 1149 1012]]
+0.14927138377404758
+DONE ***********
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
