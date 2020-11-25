4c94782bc7ec7615bb6729c667bf22f11567fa1e
Carlos Fernandes <carlosfernandes@iamachine.europe-west2-c.c.iovirtualmachine.internal>
2020-11-25 23:30:16 +0000

    Scenario 4 - RandomForest

---
+++ classification/10parks_1hour_scenario4/RandomForest.py
@@ -0,0 +1,140 @@
+'''
+
+    MULTI-CLASS CLASSIFICATION
+    Refers to those classification tasks that have more than two class labels.
+
+    Unlike binary classification, multi-class classification does not have the notion of normal and abnormal outcomes.
+    Instead, examples are classified as belonging to one among a range of known classes
+
+    Popular algorithms that can be used for mult-class classification include:
+        . KNN
+        . Decision trees
+        . Naive Bayes
+        . Random Forest
+        . Gradient Boosting
+'''
+import mysql.connector
+from datetime import datetime
+import pytz
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+import matplotlib.pyplot as plt
+import pymysql
+
+db_connection_str = 'mysql+pymysql://root:test@localhost/dissertation'
+db_connection = create_engine(db_connection_str)
+
+accepted_columns = ['parkName', 'day', 'month', 'year', 'hour', 'weekDay', 'occupancy', 'isWeekend', 'isHoliday',
+                    'precipIntensity', 'precipProbability', 'temperature', 'apparentTemperature',
+                    'dewPoint', 'humidity', 'pressure', 'windSpeed', 'windGust', 'windBearing', 'cloudCover',
+                    'uvIndex', 'visibility', 'place1Type', 'place2Type', 'place3Type', 'place4Type', 'place5Type']
+
+
+drop_cols = []
+
+dataset = pd.read_sql('SELECT * FROM final_data_10parks_1hour', con=db_connection)
+
+for col in dataset.columns.tolist():
+    if col not in accepted_columns:
+        drop_cols.append(col)
+
+## REMOVE UNUSED COLS
+dataset.drop(columns=drop_cols, inplace=True, axis=1)
+
+## SET CLASSES ACCORDING OCCUPANCY
+dataset['class'] = pd.cut(dataset['occupancy'], bins=10)
+
+
+dataset.drop(columns=['occupancy'], inplace=True, axis=1)
+
+## ENCODING CATEGORICAL DATA WITH DUMMY APPROACH
+dataset = pd.get_dummies(dataset, columns=['parkName', 'place1Type', 'place2Type', 'place3Type',
+                                           'place4Type', 'place5Type'], drop_first=True)
+
+## ENCODE TARGET VARIABLE WITH LABEL ENCODER
+from sklearn.preprocessing import LabelEncoder
+lb_make = LabelEncoder()
+dataset['class_code'] = lb_make.fit_transform(dataset['class'])
+
+
+# DROP COLUMN CLASS
+dataset.drop(columns=['class'], inplace=True, axis=1)
+
+print(list(dataset.columns))
+
+##TAKING CARE OF MISSING DATA
+dataset['precipIntensity'].fillna(method='pad', inplace=True)
+dataset['precipProbability'].fillna(method='pad', inplace=True)
+dataset['temperature'].fillna(method='pad', inplace=True)
+dataset['apparentTemperature'].fillna(method='pad', inplace=True)
+dataset['windSpeed'].fillna(method='pad', inplace=True)
+dataset['humidity'].fillna(method='pad', inplace=True)
+dataset['pressure'].fillna(method='pad', inplace=True)
+dataset['windGust'].fillna(method='pad', inplace=True)
+dataset['windBearing'].fillna(method='pad', inplace=True)
+dataset['cloudCover'].fillna(method='pad', inplace=True)
+dataset['uvIndex'].fillna(method='pad', inplace=True)
+dataset['visibility'].fillna(method='pad', inplace=True)
+
+
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+print("split dataset")
+## SPLITIING INTO TRAINING SET AND TEST SET
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+
+print("training model")
+# TRAINING THE RANDOM FOREST CLASSIFICATION MODEL ON THE TRAINING SET
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators=100, criterion='entropy', random_state=0)
+classifier.fit(X_train, y_train)
+
+
+print("predicting result")
+## PREDICTING THE TEST SET RESULT
+y_pred = classifier.predict(X_test)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+
+print("MAKING CONFUSION MATRIX")
+##MAKING THE CONFUSION MATRIX
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+print(accuracy_score(y_test, y_pred))
+print("DONE ***********")
+
+'''
+MAKING CONFUSION MATRIX
+[[   1    1    5   14   27   40   49   37   30   19]
+ [   1    6   17   46   89  174  208  144   87   67]
+ [  10   13   43  138  304  492  605  459  281  240]
+ [  13   45  140  327  711 1158 1387 1107  680  565]
+ [  25   80  223  634 1319 2159 2446 2145 1215  990]
+ [  34  104  305  887 1844 3149 3659 3061 1749 1468]
+ [  39  119  386  958 2061 3498 4020 3282 2039 1607]
+ [  37  106  349  883 1797 3099 3422 3000 1809 1470]
+ [  17   68  239  591 1336 2154 2444 2039 1215 1013]
+ [  19   75  206  562 1158 1839 2170 1812 1081  866]]
+0.15132214277188832
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
