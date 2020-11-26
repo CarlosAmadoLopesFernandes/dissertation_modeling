036cec634326796581420550409fa33d57c478cb
Carlos Fernandes <carlosfernandes@iamachine.europe-west2-c.c.iovirtualmachine.internal>
2020-11-26 23:28:22 +0000

    Scenario 7 - RandomForest

---
+++ classification/10parks_1hour_scenario7/RandomForest.py
@@ -0,0 +1,125 @@
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+
+
+db_connection_str = 'mysql+pymysql://root:test@localhost/dissertation'
+db_connection = create_engine(db_connection_str)
+
+accepted_columns = ['parkName', 'day', 'month', 'year', 'hour', 'weekDay', 'occupancy', 'isWeekend', 'isHoliday',
+                    'precipIntensity', 'precipProbability', 'temperature', 'apparentTemperature',
+                    'dewPoint', 'humidity', 'pressure', 'windSpeed', 'windGust', 'windBearing', 'cloudCover',
+                    'uvIndex', 'visibility', 'place1Type', 'place2Type', 'place3Type', 'place4Type', 'place5Type',
+                    'parkingGroupName', 'numberOfSpaces', 'handicappedPlaces', 'hourlyPrice']
+
+drop_cols = []
+
+dataset = pd.read_sql('SELECT * FROM final_data_10parks_1hour', con=db_connection)
+
+for col in dataset.columns.tolist():
+    if col not in accepted_columns:
+        drop_cols.append(col)
+
+## REMOVE UNUSED COLS
+dataset.drop(columns=drop_cols, inplace=True, axis=1)
+
+## SET CLASSES ACCORDING OCCUPANCY
+dataset['class'] = pd.cut(dataset['occupancy'], bins=10)
+
+
+dataset.drop(columns=['occupancy'], inplace=True, axis=1)
+
+## ENCODING CATEGORICAL DATA WITH DUMMY APPROACH
+dataset = pd.get_dummies(dataset, columns=['parkName', 'parkingGroupName', 'place1Type', 'place2Type', 'place3Type',
+                                           'place4Type', 'place5Type'], drop_first=True)
+
+## ENCODE TARGET VARIABLE WITH LABEL ENCODER
+from sklearn.preprocessing import LabelEncoder
+lb_make = LabelEncoder()
+dataset['class_code'] = lb_make.fit_transform(dataset['class'])
+
+
+# DROP COLUMN CLASS
+dataset.drop(columns=['class'], inplace=True, axis=1)
+
+print(list(dataset.columns))
+
+##TAKING CARE OF MISSING DATA
+dataset['precipIntensity'].fillna(method='pad', inplace=True)
+dataset['precipProbability'].fillna(method='pad', inplace=True)
+dataset['temperature'].fillna(method='pad', inplace=True)
+dataset['apparentTemperature'].fillna(method='pad', inplace=True)
+dataset['windSpeed'].fillna(method='pad', inplace=True)
+dataset['humidity'].fillna(method='pad', inplace=True)
+dataset['pressure'].fillna(method='pad', inplace=True)
+dataset['windGust'].fillna(method='pad', inplace=True)
+dataset['windBearing'].fillna(method='pad', inplace=True)
+dataset['cloudCover'].fillna(method='pad', inplace=True)
+dataset['uvIndex'].fillna(method='pad', inplace=True)
+dataset['visibility'].fillna(method='pad', inplace=True)
+
+## WHEN IS NULL, REPLACE BY ZERO
+dataset['handicappedPlaces'].fillna(0, inplace=True)
+
+
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+print("split dataset")
+## SPLITIING INTO TRAINING SET AND TEST SET
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+print("training model")
+# TRAINING THE RANDOM FOREST CLASSIFICATION MODEL ON THE TRAINING SET
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators=100, criterion='entropy', random_state=0)
+classifier.fit(X_train, y_train)
+
+
+print("predicting result")
+## PREDICTING THE TEST SET RESULT
+y_pred = classifier.predict(X_test)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+
+print("MAKING CONFUSION MATRIX")
+##MAKING THE CONFUSION MATRIX
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+print(accuracy_score(y_test, y_pred))
+print("DONE ***********")
+
+'''
+MAKING CONFUSION MATRIX
+[[   0    0    1    7   20   55   61   42   19   18]
+ [   0    3    6   24   89  185  227  181   73   51]
+ [   1    4   12   72  291  573  702  541  225  164]
+ [   6   14   49  177  632 1363 1684 1216  556  436]
+ [   7   22   72  338 1167 2477 2975 2450  978  750]
+ [   9   21  108  450 1679 3550 4554 3409 1421 1059]
+ [  10   36  122  528 1801 3972 5034 3764 1601 1141]
+ [   8   23  126  519 1576 3449 4416 3426 1406 1023]
+ [   9   17   78  354 1163 2386 3112 2354  960  683]
+ [   8   18   79  304 1031 2174 2650 2054  843  627]]
+0.16228122524712188
+DONE ***********
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
