011c003b789861c0d3c2268922285f8eebfe25bb
Carlos Fernandes <carlosfernandes@iamachine.europe-west2-c.c.iovirtualmachine.internal>
2020-11-26 22:34:38 +0000

    Scenario 6 - GradientBoosting

---
+++ classification/10parks_1hour_scenario6/GradientBoosting.py
@@ -0,0 +1,132 @@
+
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+
+db_connection_str = 'mysql+pymysql://root:test@localhost/dissertation'
+db_connection = create_engine(db_connection_str)
+
+accepted_columns = ['parkName', 'day', 'month', 'year', 'hour', 'weekDay', 'occupancy', 'isWeekend',
+                    'precipIntensity', 'precipProbability', 'temperature', 'apparentTemperature',
+                    'dewPoint', 'humidity', 'pressure', 'windSpeed', 'windGust', 'windBearing', 'cloudCover',
+                    'uvIndex', 'visibility', 'place1Type', 'place2Type', 'place3Type', 'place4Type', 'place5Type',
+                    'parkingGroupName', 'numberOfSpaces', 'handicappedPlaces', 'hourlyPrice',
+                    'has_car_washing', 'has_bike_sharing', 'has_bike_parking', 'has_elevator', 'has_information_point', 'has_toilet',
+                    'has_petrol_station', 'has_shop', 'has_restaurant', 'has_electric_charging_station', 'has_overnight_accommodation',
+                    'has_kiosk', 'has_pharmacy', 'has_cafe', 'has_medical_facility', 'has_vending_machine', 'has_spare_parts_shopping']
+
+
+drop_cols = []
+
+dataset = pd.read_sql('SELECT * FROM final_data_10parks_1hour', con=db_connection)
+
+for col in dataset.columns.tolist():
+    if col not in accepted_columns:
+        drop_cols.append(col)
+
+## REMOVE UNUSED COLS
+dataset.drop(columns=drop_cols, inplace=True, axis=1)
+
+## SET CLASSES ACCORDING OCCUPANCY
+dataset['class'] = pd.cut(dataset['occupancy'], bins=10)
+
+
+dataset.drop(columns=['occupancy'], inplace=True, axis=1)
+
+## ENCODING CATEGORICAL DATA WITH DUMMY APPROACH
+dataset = pd.get_dummies(dataset, columns=['parkName', 'parkingGroupName', 'place1Type', 'place2Type', 'place3Type',
+                                           'place4Type', 'place5Type'], drop_first=True)
+
+## ENCODE TARGET VARIABLE WITH LABEL ENCODER
+from sklearn.preprocessing import LabelEncoder
+lb_make = LabelEncoder()
+dataset['class_code'] = lb_make.fit_transform(dataset['class'])
+
+
+# DROP COLUMN CLASS
+dataset.drop(columns=['class'], inplace=True, axis=1)
+
+print(list(dataset.columns))
+
+##TAKING CARE OF MISSING DATA
+dataset['precipIntensity'].fillna(method='pad', inplace=True)
+dataset['precipProbability'].fillna(method='pad', inplace=True)
+dataset['temperature'].fillna(method='pad', inplace=True)
+dataset['apparentTemperature'].fillna(method='pad', inplace=True)
+dataset['windSpeed'].fillna(method='pad', inplace=True)
+dataset['humidity'].fillna(method='pad', inplace=True)
+dataset['pressure'].fillna(method='pad', inplace=True)
+dataset['windGust'].fillna(method='pad', inplace=True)
+dataset['windBearing'].fillna(method='pad', inplace=True)
+dataset['cloudCover'].fillna(method='pad', inplace=True)
+dataset['uvIndex'].fillna(method='pad', inplace=True)
+dataset['visibility'].fillna(method='pad', inplace=True)
+
+## WHEN IS NULL, REPLACE BY ZERO
+dataset['handicappedPlaces'].fillna(0, inplace=True)
+
+
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+print("split dataset")
+## SPLITIING INTO TRAINING SET AND TEST SET
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+print("training model")
+# TRAINING THE GRADIENT BOOST CLASSIFICATION MODEL ON THE TRAINING SET
+from sklearn.ensemble import GradientBoostingClassifier
+classifier = GradientBoostingClassifier(n_estimators=10, criterion='friedman_mse', loss='deviance', learning_rate=0.1, random_state=0)
+classifier.fit(X_train, y_train)
+
+
+print("predicting result")
+## PREDICTING THE TEST SET RESULT
+y_pred = classifier.predict(X_test)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+
+print("MAKING CONFUSION MATRIX")
+##MAKING THE CONFUSION MATRIX
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+print(accuracy_score(y_test, y_pred))
+print("DONE ***********")
+
+'''
+
+MAKING CONFUSION MATRIX
+[[    0     0     0     0     0     2   221     0     0     0]
+ [    0     0     0     0     0     2   829     8     0     0]
+ [    0     0     0     0     1     2  2573     9     0     0]
+ [    0     1     0     0     0    11  6105    13     1     2]
+ [    0     2     0     0     0    16 11190    25     2     1]
+ [    2     2     0     0     3    23 16191    35     2     2]
+ [    2     0     0     0     3    38 17922    37     4     3]
+ [    0     1     0     0     5    33 15891    36     2     4]
+ [    4     0     1     0     1    11 11061    34     1     3]
+ [    2     0     1     0     2    23  9727    30     2     1]]
+0.1951259209426981
+DONE ***********
+
+Process finished with exit code 0
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
