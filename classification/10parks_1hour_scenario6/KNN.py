6b70e5479f382c2f66eac3c370fc270367d2768a
Carlos Fernandes <carlosfernandes@iamachine.europe-west2-c.c.iovirtualmachine.internal>
2020-11-26 22:37:55 +0000

    Scenario 6 - KNN

---
+++ classification/10parks_1hour_scenario6/KNN.py
@@ -0,0 +1,133 @@
+
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+
+db_connection_str = 'mysql+pymysql://root:test@localhost/dissertation'
+db_connection = create_engine(db_connection_str)
+
+accepted_columns = ['parkName', 'day', 'month', 'year', 'hour', 'weekDay', 'occupancy', 'isWeekend',
+                    'precipIntensity', 'precipProbability', 'temperature', 'apparentTemperature',
+                    'dewPoint', 'humidity', 'pressure', 'windSpeed', 'windGust', 'windBearing', 'cloudCover',
+                    'uvIndex', 'visibility', 'place1Type', 'place2Type', 'place3Type', 'place4Type', 'place5Type',
+                    'parkingGroupName', 'numberOfSpaces', 'handicappedPlaces', 'hourlyPrice',
+                    'has_car_washing', 'has_bike_sharing', 'has_bike_parking', 'has_elevator', 'has_information_point', 'has_toilet',
+                    'has_petrol_station', 'has_shop', 'has_restaurant', 'has_electric_charging_station', 'has_overnight_accommodation',
+                    'has_kiosk', 'has_pharmacy', 'has_cafe', 'has_medical_facility', 'has_vending_machine', 'has_spare_parts_shopping']
+
+
+drop_cols = []
+
+dataset = pd.read_sql('SELECT * FROM final_data_10parks_1hour', con=db_connection)
+
+for col in dataset.columns.tolist():
+    if col not in accepted_columns:
+        drop_cols.append(col)
+
+## REMOVE UNUSED COLS
+dataset.drop(columns=drop_cols, inplace=True, axis=1)
+
+## SET CLASSES ACCORDING OCCUPANCY
+dataset['class'] = pd.cut(dataset['occupancy'], bins=10)
+
+
+dataset.drop(columns=['occupancy'], inplace=True, axis=1)
+
+## ENCODING CATEGORICAL DATA WITH DUMMY APPROACH
+dataset = pd.get_dummies(dataset, columns=['parkName', 'parkingGroupName', 'place1Type', 'place2Type', 'place3Type',
+                                           'place4Type', 'place5Type'], drop_first=True)
+
+## ENCODE TARGET VARIABLE WITH LABEL ENCODER
+from sklearn.preprocessing import LabelEncoder
+lb_make = LabelEncoder()
+dataset['class_code'] = lb_make.fit_transform(dataset['class'])
+
+
+# DROP COLUMN CLASS
+dataset.drop(columns=['class'], inplace=True, axis=1)
+
+print(list(dataset.columns))
+
+##TAKING CARE OF MISSING DATA
+dataset['precipIntensity'].fillna(method='pad', inplace=True)
+dataset['precipProbability'].fillna(method='pad', inplace=True)
+dataset['temperature'].fillna(method='pad', inplace=True)
+dataset['apparentTemperature'].fillna(method='pad', inplace=True)
+dataset['windSpeed'].fillna(method='pad', inplace=True)
+dataset['humidity'].fillna(method='pad', inplace=True)
+dataset['pressure'].fillna(method='pad', inplace=True)
+dataset['windGust'].fillna(method='pad', inplace=True)
+dataset['windBearing'].fillna(method='pad', inplace=True)
+dataset['cloudCover'].fillna(method='pad', inplace=True)
+dataset['uvIndex'].fillna(method='pad', inplace=True)
+dataset['visibility'].fillna(method='pad', inplace=True)
+
+## WHEN IS NULL, REPLACE BY ZERO
+dataset['handicappedPlaces'].fillna(0, inplace=True)
+
+
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+print("split dataset")
+## SPLITIING INTO TRAINING SET AND TEST SET
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+print("training model")
+# TRAINING THE KNN MODEL ON THE TRAINING SET
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors=5, metric='minkowski', p=2)
+classifier.fit(X_train, y_train)
+
+
+
+print("predicting result")
+## PREDICTING THE TEST SET RESULT
+y_pred = classifier.predict(X_test)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+
+print("MAKING CONFUSION MATRIX")
+##MAKING THE CONFUSION MATRIX
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+print(accuracy_score(y_test, y_pred))
+print("DONE ***********")
+
+'''
+MAKING CONFUSION MATRIX
+[[   1    2   12   24   28   47   38   41   16   14]
+ [   2   21   35   74  117  188  169  133   55   45]
+ [   9   32  103  253  420  513  541  409  172  133]
+ [  31  105  287  554  887 1214 1314  918  494  329]
+ [  53  173  503 1018 1724 2332 2325 1679  831  598]
+ [  84  287  735 1492 2423 3342 3491 2439 1151  816]
+ [  89  284  800 1677 2695 3628 3812 2776 1293  955]
+ [  75  256  740 1506 2371 3293 3264 2471 1132  864]
+ [  57  174  485 1070 1655 2231 2354 1699  841  550]
+ [  51  141  440  896 1460 1981 2050 1503  730  536]]
+0.14545198077277807
+DONE ***********
+
+Process finished with exit code 0
+
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
