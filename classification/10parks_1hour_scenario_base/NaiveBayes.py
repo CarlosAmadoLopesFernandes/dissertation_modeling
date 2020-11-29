daf1eb8b2df89770f4e754408a032f0b23a366f6
Carlos Fernandes <carlosfernandes@iamachine.europe-west2-c.c.iovirtualmachine.internal>
2020-11-29 17:56:15 +0000

    base scenario - Naive Bayes - cross validation added and other metrics

--- classification/10parks_1hour_scenario_base/NaiveBayes.py
+++ classification/10parks_1hour_scenario_base/NaiveBayes.py
@@ -68,7 +68,19 @@ print("split dataset")
 ## SPLITIING INTO TRAINING SET AND TEST SET
 from sklearn.model_selection import train_test_split
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+##X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+from sklearn.model_selection import KFold
+kf = KFold(n_splits=2)
+kf.get_n_splits(X)
+
+for train_index, test_index in kf.split(X):
+    print("TRAIN: ", train_index, "TEST: ", test_index)
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+
+
+
+
 
 print("training model")
 # TRAINING THE NAIVE BAYES MODEL ON THE TRAINING SET
@@ -76,43 +88,74 @@ from sklearn.naive_bayes import GaussianNB
 classifier = GaussianNB()
 classifier.fit(X_train, y_train)
 
-# PREDICTING A NEW RESULT
-##print(classifier.predict(sc.transform([[30, 87000]])))
 
 print("predicting result")
 ## PREDICTING THE TEST SET RESULT
 y_pred = classifier.predict(X_test)
 print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
 
+
+## METRICS
 print("MAKING CONFUSION MATRIX")
 ##MAKING THE CONFUSION MATRIX
-from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
 
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
 print(accuracy_score(y_test, y_pred))
-print("DONE ***********")
+
+report = classification_report(y_test, y_pred)
+print("******* classification_report *********")
+print(report)
+
+
+print("******  ACCURACY WITH CROSS VALIDATION ****** ")
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator=classifier, X=X_train, y=y_train, cv=10)
+print(list(accuracies))
+print("Accuracy: {:.2f} %".format(accuracies.mean()*100))
+print("Accuracy: %.4f (%.4f)" % (accuracies.mean(), accuracies.std()))
 
 '''
-[[   0    0    0    0   31   37   90   41   16    8]
- [   0    0    0    0  111  148  315  190   71    4]
- [   0    0    0    0  327  449  996  535  229   49]
- [   0    0    0    0  811 1052 2392 1280  500   98]
- [   0    0    0    0 1447 1811 4493 2347  941  197]
- [   0    0    0    0 2144 2696 6427 3392 1316  285]
- [   0    0    0    0 2376 2903 7209 3785 1449  287]
- [   0    0    0    0 2016 2616 6352 3331 1368  289]
- [   0    0    0    0 1472 1855 4337 2343  920  189]
- [   0    0    0    0 1245 1650 3898 1985  814  196]]
-0.17142826141209405
-DONE ***********
+MAKING CONFUSION MATRIX
+[[    0     0     0     0    22   302   153    34    14     2]
+ [    0     0     0     0    54   987   487    96    35     0]
+ [    0     0     0     0   197  2909  1564   269   101     1]
+ [    0     0     0     0   448  7097  3741   665   271     7]
+ [    0     0     0     0   833 13154  6990  1154   516     8]
+ [    0     0     0     0  1159 18576 10051  1688   768     8]
+ [    0     0     0     0  1347 20954 11221  1818   788    16]
+ [    0     0     0     0  1142 18617 10013  1703   700    17]
+ [    0     0     0     0   835 12735  6999  1182   488     7]
+ [    0     0     0     0   768 11141  6023   965   475     6]]
+0.17809690702632908
 
-Process finished with exit code 0
+******* classification_report *********
+              precision    recall  f1-score   support
+
+           0       0.00      0.00      0.00       527
+           1       0.00      0.00      0.00      1659
+           2       0.00      0.00      0.00      5041
+           3       0.00      0.00      0.00     12229
+           4       0.12      0.04      0.06     22655
+           5       0.17      0.58      0.27     32250
+           6       0.20      0.31      0.24     36144
+           7       0.18      0.05      0.08     32192
+           8       0.12      0.02      0.04     22246
+           9       0.08      0.00      0.00     19378
+
+    accuracy                           0.18    184321
+   macro avg       0.09      0.10      0.07    184321
+weighted avg       0.14      0.18      0.12    184321
+
+******  ACCURACY WITH CROSS VALIDATION ****** 
+[0.19416264308577008, 0.19524765366462324, 0.17496744791666666, 0.17447916666666666, 0.17952473958333334, 0.19325086805555555, 0.0920138888888889, 0.17415364583333334, 0.17637803819444445, 0.1732855902777778]
+Accuracy: 17.27 %
+Accuracy: 0.1727 (0.0282)
+
 
 '''
 
 import pickle
 pickle.dump(classifier, open('../../api/models/10_parks_1_hour_scenario1/naive_bayes.pkl', 'wb'))
 pickle.dump(list(dataset.columns)[:-1], open('../../api/models/10_parks_1_hour_scenario1/model_columns.pkl', 'wb'))
-
-
