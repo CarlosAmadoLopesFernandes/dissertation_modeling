43dcc41645633d5b54b5384c64be28011c16c793
Carlos Fernandes <carlosfernandes@iamachine.europe-west2-c.c.iovirtualmachine.internal>
2020-11-30 01:59:10 +0000

    scenario6 - GradientBoosting - cross validation added and other metrics

--- classification/10parks_1hour_scenario6/GradientBoosting.py
+++ classification/10parks_1hour_scenario6/GradientBoosting.py
@@ -71,9 +71,18 @@ y = dataset.iloc[:, -1].values
 
 print("split dataset")
 ## SPLITIING INTO TRAINING SET AND TEST SET
-from sklearn.model_selection import train_test_split
+##from sklearn.model_selection import train_test_split
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+##X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+from sklearn.model_selection import KFold
+kf = KFold(n_splits=2)
+kf.get_n_splits(X)
+
+for train_index, test_index in kf.split(X):
+    print("TRAIN: ", train_index, "TEST: ", test_index)
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+
 
 print("training model")
 # TRAINING THE GRADIENT BOOST CLASSIFICATION MODEL ON THE TRAINING SET
@@ -87,32 +96,66 @@ print("predicting result")
 y_pred = classifier.predict(X_test)
 print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
 
+## METRICS
 print("MAKING CONFUSION MATRIX")
 ##MAKING THE CONFUSION MATRIX
-from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
 
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
 print(accuracy_score(y_test, y_pred))
-print("DONE ***********")
+
+report = classification_report(y_test, y_pred)
+print("******* classification_report *********")
+print(report)
+
+
+print("******  ACCURACY WITH CROSS VALIDATION ****** ")
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator=classifier, X=X_train, y=y_train, cv=10)
+print(list(accuracies))
+print("Accuracy: {:.2f} %".format(accuracies.mean()*100))
+print("Accuracy: %.4f (%.4f)" % (accuracies.mean(), accuracies.std()))
 
 '''
 
 MAKING CONFUSION MATRIX
-[[    0     0     0     0     0     2   221     0     0     0]
- [    0     0     0     0     0     2   829     8     0     0]
- [    0     0     0     0     1     2  2573     9     0     0]
- [    0     1     0     0     0    11  6105    13     1     2]
- [    0     2     0     0     0    16 11190    25     2     1]
- [    2     2     0     0     3    23 16191    35     2     2]
- [    2     0     0     0     3    38 17922    37     4     3]
- [    0     1     0     0     5    33 15891    36     2     4]
- [    4     0     1     0     1    11 11061    34     1     3]
- [    2     0     1     0     2    23  9727    30     2     1]]
-0.1951259209426981
-DONE ***********
+[[    0     0     0     0     1     0   526     0     0     0]
+ [    0     0     1     0     0     0  1655     3     0     0]
+ [    1     0     0     0     0    12  5024     4     0     0]
+ [    1     0     0     0     6    17 12190    11     1     3]
+ [    0     0     0     0     5    39 22592    17     1     1]
+ [    1     2     1     1     3    61 32149    25     4     3]
+ [    1     3     1     2    11    59 36027    32     6     2]
+ [    1     6     2     2     1    58 32090    30     1     1]
+ [    1     1     1     0     4    44 22164    24     4     3]
+ [    1     2     1     1     5    41 19307    16     3     1]]
+0.1960058810444822
+******* classification_report *********
+              precision    recall  f1-score   support
+
+           0       0.00      0.00      0.00       527
+           1       0.00      0.00      0.00      1659
+           2       0.00      0.00      0.00      5041
+           3       0.00      0.00      0.00     12229
+           4       0.14      0.00      0.00     22655
+           5       0.18      0.00      0.00     32250
+           6       0.20      1.00      0.33     36144
+           7       0.19      0.00      0.00     32192
+           8       0.20      0.00      0.00     22246
+           9       0.07      0.00      0.00     19378
+
+    accuracy                           0.20    184321
+   macro avg       0.10      0.10      0.03    184321
+weighted avg       0.15      0.20      0.07    184321
+
+******  ACCURACY WITH CROSS VALIDATION ****** 
+[0.19638691477241904, 0.19638691477241904, 0.18343098958333334, 0.1960720486111111, 0.09678819444444445, 0.1962348090277778, 0.18809678819444445, 0.18912760416666666, 0.1965060763888889, 0.19596354166666666]
+Accuracy: 18.35 %
+Accuracy: 0.1835 (0.0292)
 
 Process finished with exit code 0
+
 
 '''
 
