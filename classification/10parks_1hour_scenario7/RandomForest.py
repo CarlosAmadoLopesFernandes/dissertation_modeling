342776271a7244a942ecd81a6ab582d7948b1e75
Carlos Fernandes <carlosfernandes@iamachine.europe-west2-c.c.iovirtualmachine.internal>
2020-11-30 02:27:50 +0000

    scenario7 - RandomForest - cross validation added and other metrics

--- classification/10parks_1hour_scenario7/RandomForest.py
+++ classification/10parks_1hour_scenario7/RandomForest.py
@@ -67,9 +67,18 @@ y = dataset.iloc[:, -1].values
 
 print("split dataset")
 ## SPLITIING INTO TRAINING SET AND TEST SET
-from sklearn.model_selection import train_test_split
+##from sklearn.model_selection import train_test_split
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+##X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+from sklearn.model_selection import KFold
+kf = KFold(n_splits=2)
+kf.get_n_splits(X)
+
+for train_index, test_index in kf.split(X):
+    print("TRAIN: ", train_index, "TEST: ", test_index)
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+
 
 print("training model")
 # TRAINING THE RANDOM FOREST CLASSIFICATION MODEL ON THE TRAINING SET
@@ -83,29 +92,64 @@ print("predicting result")
 y_pred = classifier.predict(X_test)
 print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
 
+## METRICS
 print("MAKING CONFUSION MATRIX")
 ##MAKING THE CONFUSION MATRIX
-from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
 
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
 print(accuracy_score(y_test, y_pred))
-print("DONE ***********")
+
+report = classification_report(y_test, y_pred)
+print("******* classification_report *********")
+print(report)
+
+
+print("******  ACCURACY WITH CROSS VALIDATION ****** ")
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator=classifier, X=X_train, y=y_train, cv=10)
+print(list(accuracies))
+print("Accuracy: {:.2f} %".format(accuracies.mean()*100))
+print("Accuracy: %.4f (%.4f)" % (accuracies.mean(), accuracies.std()))
 
 '''
 MAKING CONFUSION MATRIX
-[[   0    0    1    7   20   55   61   42   19   18]
- [   0    3    6   24   89  185  227  181   73   51]
- [   1    4   12   72  291  573  702  541  225  164]
- [   6   14   49  177  632 1363 1684 1216  556  436]
- [   7   22   72  338 1167 2477 2975 2450  978  750]
- [   9   21  108  450 1679 3550 4554 3409 1421 1059]
- [  10   36  122  528 1801 3972 5034 3764 1601 1141]
- [   8   23  126  519 1576 3449 4416 3426 1406 1023]
- [   9   17   78  354 1163 2386 3112 2354  960  683]
- [   8   18   79  304 1031 2174 2650 2054  843  627]]
-0.16228122524712188
-DONE ***********
+[[   1    0    9   25   79  100  107  109   53   44]
+ [   2   13   25   82  198  326  369  311  189  144]
+ [  13   35   84  262  597  941 1181  942  555  431]
+ [  19   83  238  621 1446 2313 2808 2355 1307 1039]
+ [  31  126  396 1166 2607 4455 5131 4235 2511 1997]
+ [  51  189  628 1625 3854 6332 7249 6015 3501 2806]
+ [  73  230  655 1804 4159 7026 8274 6788 3989 3146]
+ [  54  182  602 1663 3635 6192 7295 6250 3486 2833]
+ [  31  129  418 1044 2601 4184 5189 4174 2541 1935]
+ [  36  106  341  954 2248 3778 4439 3648 2122 1706]]
+0.15423635939475155
+******* classification_report *********
+              precision    recall  f1-score   support
+
+           0       0.00      0.00      0.00       527
+           1       0.01      0.01      0.01      1659
+           2       0.02      0.02      0.02      5041
+           3       0.07      0.05      0.06     12229
+           4       0.12      0.12      0.12     22655
+           5       0.18      0.20      0.19     32250
+           6       0.20      0.23      0.21     36144
+           7       0.18      0.19      0.19     32192
+           8       0.13      0.11      0.12     22246
+           9       0.11      0.09      0.10     19378
+
+    accuracy                           0.15    184321
+   macro avg       0.10      0.10      0.10    184321
+weighted avg       0.15      0.15      0.15    184321
+
+******  ACCURACY WITH CROSS VALIDATION ****** 
+[0.1600390603808387, 0.1610698204307492, 0.15370008680555555, 0.15733506944444445, 0.1393771701388889, 0.1443142361111111, 0.11675347222222222, 0.14794921875, 0.1460503472222222, 0.1467556423611111]
+Accuracy: 14.73 %
+Accuracy: 0.1473 (0.0122)
+
+Process finished with exit code 0
 
 '''
 
